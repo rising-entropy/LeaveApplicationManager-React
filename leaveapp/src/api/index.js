7d00bdde27d1924378fa9caab9ebacd2addc913e
Devang Kamble <risingentropy20@gmail.com>
2021-07-31 23:03:44 +0530

    getting api to work in saga

--- leaveapp/src/api/index.js
+++ leaveapp/src/api/index.js
@@ -20,23 +20,23 @@ export const loginSubmit = async (body) => {
           console.log(data);
           if (data.status === 404) {
             alert(data.message);
-            window.location = "/login";
+            window.location = "/";
             return 0;
           }
           if (data.status === 403) {
             alert(data.message);
-            window.location = "/login";
+            window.location = "/";
             return 0;
           }
           localStorage.setItem("username", data.username);
           localStorage.setItem("token", data.token);
           alert("Successfully Logged In")
-          window.location = "/feed";
+          window.location = "/";
         }
       })
       .catch((err) => {
         alert("Server Seems to be down. Please try later. We got this.");
-        window.location = '/login'
+        window.location = '/'
         //throw err
       });
 }
