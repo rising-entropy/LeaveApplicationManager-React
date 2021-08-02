8db63c575763a0620159c1bf9ea84c5b0c20a4eb
Devang Kamble <risingentropy20@gmail.com>
2021-08-02 16:57:22 +0530

    stuff is rendering

--- leaveapp/src/components/AllApplications/Application.js
+++ leaveapp/src/components/AllApplications/Application.js
@@ -5,12 +5,13 @@ export default class Application extends Component {
     
     render() {
         return (
-            <div className="card col-lg-4 col-md-6 col-sm-12" style={{margin: "4% auto"}}>
-                <h5 className="card-header">Featured</h5>
+            <div className="card col-lg-4 col-md-6 col-sm-12 text-center" style={{margin: "4% auto"}}>
+                <h5 className="card-header">{this.props.application.leaveType}</h5>
                 <div className="card-body">
-                    <h5 className="card-title">Special title treatment</h5>
-                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                    <h5 className="card-title" style={{margin: "20px auto"}}>{this.props.application.leaveFrom} - {this.props.application.leaveTo}</h5>
+                    {this.props.application.documents.map((doc, index)=>(
+                        <a href={doc} className="btn btn-primary" key={index} rel="noreferrer" target="_blank" style={{margin: "3% 2%"}}>Doc {index+1}</a>
+                    ))}
                 </div>
             </div>
         )
